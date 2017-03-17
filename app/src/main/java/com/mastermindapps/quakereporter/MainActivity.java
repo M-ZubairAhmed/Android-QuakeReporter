3d046c508b073c16b5acd5af369d8ae2531e63d5
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-17 13:11:23 +0530

    added placeholder data to list

--- app/src/main/java/com/mastermindapps/quakereporter/MainActivity.java
+++ app/src/main/java/com/mastermindapps/quakereporter/MainActivity.java
@@ -2,12 +2,23 @@ package com.mastermindapps.quakereporter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<MainEarthquareData> arrayListMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        arrayListMain = new ArrayList<>();
+        arrayListMain.add(new MainEarthquareData("5.2","Mosco, Russia", "24 Apr 16"));
+        arrayListMain.add(new MainEarthquareData("2.1","Java, Indonesia", "21 Apr 16"));
+        ListView listView = (ListView) findViewById(R.id.list_view_xml);
+
     }
 }
