be63fb500835ccddaf0cd616b41d0bca0f82d43e
M-ZubairAhmed <ahmedzubair216@gmail.com>
2017-03-17 14:37:53 +0530

    set placeholder adapter

--- app/src/main/java/com/mastermindapps/quakereporter/MainActivity.java
+++ app/src/main/java/com/mastermindapps/quakereporter/MainActivity.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<MainEarthquareData> arrayListMain;
+    private MainEarthquakeAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +19,10 @@ public class MainActivity extends AppCompatActivity {
         arrayListMain = new ArrayList<>();
         arrayListMain.add(new MainEarthquareData("5.2","Mosco, Russia", "24 Apr 16"));
         arrayListMain.add(new MainEarthquareData("2.1","Java, Indonesia", "21 Apr 16"));
-        ListView listView = (ListView) findViewById(R.id.list_view_xml);
+        arrayListMain.add(new MainEarthquareData("5.0","Mumbai,India", "11 Apr 16"));
+        adapter = new MainEarthquakeAdapter(this,arrayListMain);
 
+        ListView listView = (ListView) findViewById(R.id.list_view_xml);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/mastermindapps/quakereporter/MainEarthquakeAdapter.java
+++ app/src/main/java/com/mastermindapps/quakereporter/MainEarthquakeAdapter.java
@@ -1,7 +1,38 @@
 package com.mastermindapps.quakereporter;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
-public class MainEarthquakeAdapter extends ArrayAdapter {
+import java.util.ArrayList;
 
+public class MainEarthquakeAdapter extends ArrayAdapter<MainEarthquareData> {
+
+    public MainEarthquakeAdapter(@NonNull Context context, @NonNull ArrayList<MainEarthquareData> arrayListMain) {
+        super(context, 0, arrayListMain);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View rowView = convertView;
+        if (rowView == null){
+            rowView = LayoutInflater.from(getContext()).inflate(R.layout.row_main,parent,false);
+        }
+        MainEarthquareData item = getItem(position);
+
+        TextView ritcher_text = (TextView) rowView.findViewById(R.id.row_main_ritcher_xml);
+        ritcher_text.setText(item.getRitcher());
+        TextView place_text = (TextView) rowView.findViewById(R.id.row_main_city_xml);
+        place_text.setText(item.getPlace());
+        TextView date_text = (TextView) rowView.findViewById(R.id.row_main_date_xml);
+        date_text.setText(item.getDate());
+
+        return rowView;
+    }
 }
